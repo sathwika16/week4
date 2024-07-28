5a327a457167af000cb23a094ca63e3476503493
sathwika16 <sathwikakunta556@gmail.com>
2024-07-28 16:10:38 +0530

    urls.py

--- tweets/urls.py
+++ tweets/urls.py
@@ -4,7 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.tweet_list, name='tweet_list'),
     path('create/', views.create_tweet, name='create_tweet'),
-    path('edit/<int:pk>/', views.tweet_edit, name='tweet_edit'),  
-    path('delete/<int:pk>/', views.tweet_delete, name='tweet_delete'),  
+    path('<int:pk>/edit/', views.tweet_edit, name='tweet_edit'),
+    path('<int:pk>/delete/', views.tweet_delete, name='tweet_delete'),
     path('search/', views.tweet_search, name='tweet_search'),
 ]
